013dd032a391d5d89600f9f3979bf36787dcc56c
Nathan <130991624+NathanCart@users.noreply.github.com>
2024-05-21 21:07:58 +0100

    Bug fix

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,32 @@
+import projects from '@/utils/projects';
+import Header from '../components/Header';
 import Homepage from '../components/Homepage';
+import Card from '../components/Card';
+import FloatIntoScreen from '../components/FloatIntoScreen';
 
 export default function Projects() {
-	return <>Projects page...</>;
+	return (
+		<div className="bg-neutral-100 overflow-hidden">
+			<section
+				id="projects"
+				className="relative container mx-auto p-4 text-center py-8 mt-4 bg-neutral-100 z-30"
+			>
+				<FloatIntoScreen direction="left" y={100} bounce={0.4}>
+					<div className="flex flex-col md:flex-row items-start md:items-end md:gap-2">
+						<h1 className="text-2xl md:text-3xl font-sans font-extrabold">Projects</h1>
+						<p className="md:text-lg font-mono">Explore all my projects!</p>
+					</div>
+				</FloatIntoScreen>
+				<div className="grid grid-cols-12 mt-4 text-start gap-4">
+					{projects.map((project, index) => {
+						return (
+							<div key={index} className="col-span-12 md:col-span-6 lg:col-span-4">
+								<Card {...project} />
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		</div>
+	);
 }
--- utils/projects/aramaKarmaVeygo.ts
+++ utils/projects/aramaKarmaVeygo.ts
@@ -12,7 +12,7 @@ const armaKarmaVeygo: Project = {
 	technologies: ['nextjs-logo.svg', 'mui-logo.png', 'nodejs-logo.svg', 'express-logo.svg'],
 	how: 'Next.js was used to build the frontend of the website. Material-UI was used for the styling of the website. Node.js and Express.js were used to build the backend of the website. The website is split using environment variables to allow for different configurations for different clients. For example, we have a different configuration for Veygo and Arma Karma for different styling.',
 	conclusion:
-		'By utilising Next.js, Material-UI, Node.js, and Express.js, we were able to build a website that met the client requirements. The website is able to support different clients with different configurations and provides a seamless user experience which allows users to easily select items to insure. ',
+		'By utilising Next.js, Material-UI, Node.js, and Express.js, we were able to build a website that met the client requirements. The website is able to support different clients with different configurations and provides a seamless user experience which allows users to easily select items to insure.',
 };
 
 export default armaKarmaVeygo;
